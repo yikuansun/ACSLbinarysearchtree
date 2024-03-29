227c5730000438ef9012ba3c0685ab918c4a1afe
Yikuan Sun <yikuansun2015@gmail.com>
2024-03-29 14:43:18 -0400

    Create tree drawing function
    kinda sucks but eh

--- code.js
+++ code.js
@@ -48,13 +48,39 @@ class tTree {
         }
         return rec(this.root, 0);
     }
+
+    render() {
+        let renderNode = function(n) {
+            let table = document.createElement("table");
+            table.style.textAlign = "center";
+            let r1 = document.createElement("tr");
+            let topNodeRendered = document.createElement("td");
+            topNodeRendered.setAttribute("colspan", 2);
+            topNodeRendered.innerText = n.id;
+            r1.appendChild(topNodeRendered);
+            table.appendChild(r1);
+            let r2 = document.createElement("tr");
+            let child0Rendered = document.createElement("td");
+            child0Rendered.style.verticalAlign = "top";
+            if (n.children[0]) child0Rendered.appendChild(renderNode(n.children[0]));
+            r2.appendChild(child0Rendered);
+            let child1Rendered = document.createElement("td");
+            child1Rendered.style.verticalAlign = "top";
+            if (n.children[1]) child1Rendered.appendChild(renderNode(n.children[1]));
+            r2.appendChild(child1Rendered);
+            table.appendChild(r2);
+            return table;
+        }
+        let treeRendered = renderNode(this.root);
+        return treeRendered;
+    }
 }
 
-let x = new tTree("a");
-x.addLetter("m");
-x.addLetter("e");
+let x = new tTree("p");
 x.addLetter("r");
-x.addLetter("i");
-x.addLetter("c");
+x.addLetter("o");
+x.addLetter("g");
+x.addLetter("r");
 x.addLetter("a");
-x.addLetter("n");
+x.addLetter("m");
+x.render();
