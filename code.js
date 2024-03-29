83f61cdeb6ed6d1ae308f3a10243ef5bad8a7a74
Yikuan Sun <yikuansun2015@gmail.com>
2024-03-29 14:30:15 -0400

    Calculate internal path length

--- code.js
+++ code.js
@@ -30,4 +30,31 @@ class tTree {
     addLetter(l) {
         this.root.addNode(new tNode(l));
     }
+
+    get internalPathLength() {
+        let rec = function(startNode, startDepth) {
+            if (!startNode.children[0] && !startNode.children[1]) {
+                return startDepth;
+            }
+            else if (startNode.children[0] && !startNode.children[1]) {
+                return startDepth + rec(startNode.children[0], startDepth + 1);
+            }
+            else if (!startNode.children[0] && startNode.children[1]) {
+                return startDepth + rec(startNode.children[1], startDepth + 1);
+            }
+            else { // startNode.children[0] && startNode.children[1]
+                return startDepth + rec(startNode.children[0], startDepth + 1) + rec(startNode.children[1], startDepth + 1);
+            }
+        }
+        return rec(this.root, 0);
+    }
 }
+
+let x = new tTree("a");
+x.addLetter("m");
+x.addLetter("e");
+x.addLetter("r");
+x.addLetter("i");
+x.addLetter("c");
+x.addLetter("a");
+x.addLetter("n");
